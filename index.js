ea31a15f75a1a2d78e1edaf6edd36674c44edab2
Andrii Kostetskyi <kostyandrew@gmail.com>
2017-03-30 01:05:46 +0300

    Ukrainian translation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const HowToMarkdown = workshopper({
   appDir: __dirname,
-  languages: ['en', 'zh-cn'],
+  languages: ['en', 'zh-cn', 'uk'],
   header: require('workshopper-adventure/default/header'),
   footer: [{
     file: path.join(__dirname, 'i18n', 'footer', '{lang}.md'),
